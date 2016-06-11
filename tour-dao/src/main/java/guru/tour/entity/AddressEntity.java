2d96d27dc559fb0b43de0c1c392c731461791916
HP <lekhanhduyduy1201@gmail.com>
2016-06-11 09:44:44 -0700

    check

---
+++ tour-dao/src/main/java/guru/tour/entity/AddressEntity.java
@@ -0,0 +1,52 @@
+package guru.tour.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class AddressEntity implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	@Column(name = "address")
+	private String address;
+	
+	
+	public AddressEntity() {
+		super();
+	}
+	
+	
+	public AddressEntity(int id, String address) {
+		super();
+		this.id = id;
+		this.address = address;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
