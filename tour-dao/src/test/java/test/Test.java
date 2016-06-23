8bba6eafdba22fd79b9459d871ddceb0d84c4c59
vantrongtran <vntrngtrn@yahoo.com.vn>
2016-06-23 20:47:24 +0700

    test

---
+++ tour-dao/src/test/java/test/Test.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import guru.tour.entity.AddressEntity;
+import guru.tour.service.AddressEntityManager;
+
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class Test {
+	@Autowired
+	AddressEntityManager add;
+	
+	@org.junit.Test
+	public void show(){
+		List<AddressEntity> list = add.getAllAdress();
+		for (AddressEntity addressEntity : list) {
+			System.out.println(addressEntity.getAddress());
+		}
+	}
+}
