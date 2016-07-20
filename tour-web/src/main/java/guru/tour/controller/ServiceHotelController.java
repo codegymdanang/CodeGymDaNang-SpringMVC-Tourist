76995f212f2631b697c876d0bc17ae4f48564800
Bin <kyhuuhai@gmail.com>
2016-07-20 10:15:57 +0700

    Hotel Exception &&& http://www.journaldev.com/2651/spring-mvc-exception-handling-controlleradvice-exceptionhandler-handlerexceptionresolver

--- tour-web/src/main/java/guru/tour/controller/ServiceHotelController.java
+++ tour-web/src/main/java/guru/tour/controller/ServiceHotelController.java
@@ -8,11 +8,19 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import guru.tour.entity.HotelEntity;
 import guru.tour.entity.LocationEntity;
+import guru.tour.exception.HotelNotFoundException;
 import guru.tour.service.HotelEntityManager;
 import guru.tour.service.LocationEntityManager;
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -21,6 +29,8 @@ public class ServiceHotelController {
 	
 	@Autowired
 	HotelEntityManager hotel;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ServiceHotelController.class);
 	
 	@Autowired
 	LocationEntityManager location;
@@ -37,4 +47,38 @@ public class ServiceHotelController {
 		mm.addAttribute("list", list);
 		return "detailhotel";
 	}
+
+	@RequestMapping(value="/hotel/{id}", method=RequestMethod.GET)
+	public String getEmployee(@PathVariable("id") int id, Model model) throws Exception{
+		//deliberately throwing different types of exception
+		if(id==1){
+			throw new HotelNotFoundException(id);
+		}else if(id==2){
+			throw new SQLException("SQLException, id="+id);
+		}else if(id==3){
+			throw new IOException("IOException, id="+id);
+		}else if(id==10){
+			HotelEntity emp = new HotelEntity();
+			emp.setName("Pankaj");
+			emp.setId(id);
+			model.addAttribute("employee", emp);
+			return "hotel";
+		}else {
+			throw new Exception("Generic Exception, id="+id);
+		}
+		
+	}
+	
+	@ExceptionHandler(HotelNotFoundException.class)
+	public ModelAndView handleEmployeeNotFoundException(HttpServletRequest request, Exception ex){
+		logger.error("Requested URL="+request.getRequestURL());
+		logger.error("Exception Raised="+ex);
+		
+		ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.addObject("exception", ex);
+	    modelAndView.addObject("url", request.getRequestURL());
+	    
+	    modelAndView.setViewName("error");
+	    return modelAndView;
+	}	
 }
---
+++ tour-web/src/main/java/guru/tour/exception/HotelNotFoundException.java
@@ -0,0 +1,14 @@
+package guru.tour.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Hotel Not Found") //404
+public class HotelNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -3332292346834265371L;
+
+	public HotelNotFoundException(int id){
+		super("HotelNotFoundException with id="+id);
+	}
+}
