16ec49ba98d7f766b233b47bea9363d2d94cc5c3
hoanglong <hoanglonglch@gmail.com>
2016-07-20 19:41:39 -0700

    place exception

---
+++ tour-web/src/main/java/guru/tour/controller/GlobalExceptionController.java
@@ -0,0 +1,35 @@
+package guru.tour.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionController {
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionController.class);
+
+	@ExceptionHandler(SQLException.class)
+	public String handleSQLException(HttpServletRequest request, Exception ex) {
+		logger.info("SQLException Occured:: URL=" + request.getRequestURL());
+		return "database_error";
+	}
+
+	@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "IOException occured")
+	@ExceptionHandler(IOException.class)
+	public void handleIOException() {
+		logger.error("IOException handler executed");
+		// returning 404 error code
+
+	}
+
+}
--- tour-web/src/main/java/guru/tour/controller/PlaceController.java
+++ tour-web/src/main/java/guru/tour/controller/PlaceController.java
@@ -2,44 +2,72 @@ package guru.tour.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import guru.tour.entity.PlaceEntity;
+import guru.tour.exception.PlaceNotFoundException;
 import guru.tour.service.PlaceEntityManager;
 
 import guru.tour.entity.FoodEntity;
 import guru.tour.service.FoodEntityManager;
 
-
 @Controller
 @RequestMapping(value = "/")
 public class PlaceController {
-//	@Autowired
-//	HotelEntityManager hotel;
+	// @Autowired
+	// HotelEntityManager hotel;
 	@Autowired
 	PlaceEntityManager placeEntityManager;
 	@Autowired
 	FoodEntityManager foodEntityManager;
+	
+	private static final Logger logger = LoggerFactory.getLogger(PlaceController.class);
 
-	@RequestMapping(value = "/place",method = RequestMethod.GET)
-	public ModelAndView getPlaceByID(){
+	@RequestMapping(value = "/place/{id}", method = RequestMethod.GET)
+	public ModelAndView getPlaceByID(@PathVariable("id") int id) throws Exception {
 		PlaceEntity placeEntity = placeEntityManager.getPlaceByID(1);
+		if (!(id == placeEntity.getId())) {
+			throw new PlaceNotFoundException(id);
+		} else if (id == placeEntity.getId()) {
+			ModelAndView model = new ModelAndView("place");
+			model.addObject("Place", placeEntity);
+			List<FoodEntity> foodList = foodEntityManager.findAll();
+			model.addObject("list", foodList);
+			return model;
+		} else {
+			throw new Exception("Generic Exception, id=" + id);
+		}
+		/*PlaceEntity placeEntity = placeEntityManager.getPlaceByID(id);
 		System.out.println(placeEntity.getName());
 		ModelAndView model = new ModelAndView("place");
 		model.addObject("Place", placeEntity);
 		List<FoodEntity> foodList = foodEntityManager.findAll();
 		model.addObject("list", foodList);
-		return model;
+		return model;*/
 	}
-//	@RequestMapping(value = "/place",method=RequestMethod.GET)
-//	public ModelAndView getHotel() {
-//		List<HotelEntity> hotelList = hotel.getAllHotel();
-//		ModelAndView model = new ModelAndView();
-//		model.addObject("list",hotelList);
-//		return model;
-//	}
+
+	@ExceptionHandler(PlaceNotFoundException.class)
+	public ModelAndView handleEmployeeNotFoundException(HttpServletRequest request, Exception ex) {
+		logger.error("Requested URL=" + request.getRequestURL());
+		logger.error("Exception Raised=" + ex);
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("exception", ex);
+		modelAndView.addObject("url", request.getRequestURL());
+
+		modelAndView.setViewName("error");
+		return modelAndView;
+	}
+
 }
