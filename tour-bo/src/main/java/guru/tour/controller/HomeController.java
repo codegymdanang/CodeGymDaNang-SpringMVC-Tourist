e5a738be764c24ad60fd7a2810521766e73f4960
BuiNgocQuoc <BuiNgocQuoc@BuiNgocQuoc-PC>
2016-07-24 09:34:12 +0700

    tour-bo

--- tour-bo/src/main/java/guru/tour/controller/HomeController.java
+++ tour-bo/src/main/java/guru/tour/controller/HomeController.java
@@ -1,5 +1,10 @@
 package guru.tour.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+	
 }
