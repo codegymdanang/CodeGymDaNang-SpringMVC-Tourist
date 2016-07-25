ea2fbd7525555ad5d3c2b8a1ff43466e4b075cf0
hoanglong <hoanglonglch@gmail.com>
2016-07-24 19:49:35 -0700

    init bo

--- tour-bo/src/main/java/guru/tour/controller/HomeController.java
+++ tour-bo/src/main/java/guru/tour/controller/HomeController.java
@@ -2,9 +2,14 @@ package guru.tour.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/")
 public class HomeController {
 	
+	@RequestMapping(value = "/home",method = RequestMethod.GET)
+	public String homePage(){
+		return "homePage";
+	}
 }
