48855aff4a9fc532bfd4d8c2b1a395ee53e5bea7
BuiNgocQuoc <BuiNgocQuoc@BuiNgocQuoc-PC>
2016-08-02 05:23:28 +0700

    tour-bo-web

---
+++ tour-bo-web/src/main/java/guru/tour/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package guru.tour.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/")
+public class HomePageController {
+	
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String HomePage(){
+		
+		return "home";
+	}
+}
---
+++ tour-bo-web/src/main/webapp/view/admin/home.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>ahihi</h2>
+</body>
+</html>
