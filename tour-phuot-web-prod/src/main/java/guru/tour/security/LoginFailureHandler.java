6af2f309e1a928ccb118740e126f83461030f77d
ngocq <hoangbuingocquy@gmail.com>
2016-08-19 20:07:31 +0700

    security

---
+++ tour-phuot-web-prod/src/main/java/guru/tour/security/LoginFailureHandler.java
@@ -0,0 +1,21 @@
+package guru.tour.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by quy
+ */
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+       String username = request.getParameter("username");
+
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
